6ad88d3165faff1f1b0c0090724a5caad01c164f
Pranav Raj Singh Chauhan <pranav.chauhan@metricstream.com>
2017-11-27 21:24:58 +0530

    support for getting view using react tag

--- ios/RNMaterialShowcase.m
+++ ios/RNMaterialShowcase.m
@@ -1,21 +1,40 @@
-
 #import "RNMaterialShowcase.h"
 #import <MaterialShowcase/MaterialShowcase-Swift.h>
 
-
 @implementation RNMaterialShowcase
+
+@synthesize bridge = _bridge;
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
 
+
+RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(ShowSequence:(NSArray *)views props:(NSDictionary *)props)
 {
-    NSLog(@"");
     MaterialShowcase *materialShowcase = [[MaterialShowcase alloc] init];
+    UIView *sender = [self.bridge.uiManager viewForReactTag: [NSNumber numberWithInt: 5]];
+
+    
+//    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
+//        UIView *view = viewRegistry[reactTag];
+//        if (![view isKindOfClass:[RCTXXXView class]]) {
+//            RCTLogError(@"Invalid view returned from registry, expecting RCTXXXView, got: %@", view);
+//        } else {
+//            [view doSomething:str];
+//        }
+//    }];
+    
+//    UIView *sender = [self.bridge.uiManager viewForReactTag: [NSNumber numberWithInt: 5]];
+//    RCTBridge *bridge = (RCTBridge *) self.bridge;
+//    id uiManager = bridge.uiManager;
+//
+//    UIView *sender = [self.bridge.uiManager viewForReactTag: [NSNumber numberWithInt: 5]];
+
+    NSLog(@"");
 }
 
 @end
