5dae7d9d2406fc7e503468e0f4322aa35c235231
cong.nguyen <cong.nguyen@enouvo.com>
2018-03-09 09:31:57 +0700

    * format code example

--- MaterialShowcaseExample/App.js
+++ MaterialShowcaseExample/App.js
@@ -4,24 +4,24 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 import {
   AppTour,
   AppTourSequence,
-  AppTourView
-} from "react-native-material-showcase-ios";
+  AppTourView,
+} from 'react-native-material-showcase-ios';
 
-import Top from "./components/Top";
-import Center from "./components/Center";
-import Bottom from "./components/Bottom";
+import Top from './components/Top';
+import Center from './components/Center';
+import Bottom from './components/Bottom';
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
@@ -37,39 +37,49 @@ export default class App extends Component<{}> {
       this.appTourTargets.forEach(appTourTarget => {
         appTourSequence.add(appTourTarget);
       });
-
       AppTour.ShowSequence(appTourSequence);
     }, 1000);
   }
 
   render() {
-    return <View style={styles.container}>
-        <Top style={styles.top} addAppTourTarget={appTourTarget => {
+    return (
+      <View style={styles.container}>
+        <Top
+          style={styles.top}
+          addAppTourTarget={appTourTarget => {
             this.appTourTargets.push(appTourTarget);
-          }} />
-        <Center style={styles.center} addAppTourTarget={appTourTarget => {
+          }}
+        />
+        <Center
+          style={styles.center}
+          addAppTourTarget={appTourTarget => {
             this.appTourTargets.push(appTourTarget);
-          }} />
-        <Bottom style={styles.bottom} addAppTourTarget={appTourTarget => {
+          }}
+        />
+        <Bottom
+          style={styles.bottom}
+          addAppTourTarget={appTourTarget => {
             this.appTourTargets.push(appTourTarget);
-          }} />
-      </View>;
+          }}
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between"
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
   top: {
-    flex: 1
+    flex: 1,
   },
   center: {
-    flex: 1
+    flex: 1,
   },
   bottom: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
