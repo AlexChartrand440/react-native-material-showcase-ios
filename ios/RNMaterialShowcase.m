445edc5f5b2eff699f5648027792c4e6a635bb1e
cong.nguyen <cong.nguyen@enouvo.com>
2018-03-14 15:46:41 +0700

    ! update most properties from switf lib (targetHolderRadius, alignment right - center - left, ...) Now can use in objective-C, react native

--- ios/RNMaterialShowcase.m
+++ ios/RNMaterialShowcase.m
@@ -103,7 +103,6 @@ RCT_EXPORT_METHOD(ShowSequence:(NSArray *)views props:(NSDictionary *)props)
     
     NSMutableArray *viewIds = [[NSMutableArray alloc] init];
     NSMutableDictionary *props = [[NSMutableDictionary alloc] init];
-    BOOL isFinish = targetKeys.count <= 1;
     
     if (targetKeys.count <= 1) {
         [self.bridge.eventDispatcher sendDeviceEventWithName:onFinishShowStepEvent body:@{@"finish": @YES}];
@@ -141,42 +140,58 @@ RCT_EXPORT_METHOD(ShowFor:(nonnull NSNumber *)view props:(NSDictionary *)props)
     
     // Background
     UIColor *backgroundPromptColor;
-    float backgroundPromptColorAlpha = 0.96;
-    
-    // Target
-    UIColor *targetTintColor;
-    float targetHolderRadius = 44;
-    UIColor *targetHolderColor;
-    
-    // Text
-    UIColor *primaryTextColor;
-    UIColor *secondaryTextColor;
-    float primaryTextSize = 20;
-    float secondaryTextSize = 15;
-    //    showcase.primaryTextFont = UIFont.boldSystemFont(ofSize: primaryTextSize)
-    //    showcase.secondaryTextFont = UIFont.systemFont(ofSize: secondaryTextSize)
-    
-    // Animation
-    float aniComeInDuration = 0.5; // unit: second
-    float aniGoOutDuration = 0.5; // unit: second
-    float aniRippleScale = 1.5;
-    UIColor *aniRippleColor;
-    float aniRippleAlpha = 0.2;
-    
     NSString *backgroundPromptColorValue = [props objectForKey:@"backgroundPromptColor"];
     if (backgroundPromptColorValue != nil) {
         backgroundPromptColor = [UIColor fromHexWithHexString: backgroundPromptColorValue];
     }
+    if (backgroundPromptColor != nil) {
+        [materialShowcase setBackgroundColor: backgroundPromptColor];
+    }
     
+    if ([props objectForKey:@"backgroundPrompAlpha"] != nil) {
+        float backgroundPrompAlphaValue = [[props objectForKey:@"backgroundPrompAlpha"] floatValue];
+        if (backgroundPrompAlphaValue >= 0.0 && backgroundPrompAlphaValue <= 1.0) {
+            [materialShowcase setBackgroundPromptColorAlpha:backgroundPrompAlphaValue];
+        }
+    }
+    
+    // Target
+    UIColor *targetTintColor;
+    UIColor *targetHolderColor;
     NSString *targetTintColorValue = [props objectForKey:@"targetTintColor"];
     if (targetTintColorValue != nil) {
         targetTintColor = [UIColor fromHexWithHexString: targetTintColorValue];
+    }
+    
+    if (targetTintColor != nil) {
+        target.tintColor = targetTintColor;
+        [materialShowcase setTargetTintColor: targetTintColor];
+    } if (targetHolderColor != nil) {
+        [materialShowcase setTargetHolderColor: targetHolderColor];
+    }
+    
+    if ([props objectForKey:@"targetHolderRadius"] != nil) {
+        float targetHolderRadiusValue = [[props objectForKey:@"targetHolderRadius"] floatValue];
+        if (targetHolderRadiusValue >= 0) {
+            [materialShowcase setTargetHolderRadius: targetHolderRadiusValue];
+        }
     }
     
     NSString *targetHolderColorValue = [props objectForKey:@"targetHolderColor"];
     if (targetHolderColorValue != nil) {
         targetHolderColor = [UIColor fromHexWithHexString: targetHolderColorValue];
     }
+    
+    BOOL *isTapRecognizerForTagretViewValue = [[props objectForKey:@"isTapRecognizerForTagretView"] boolValue];
+    if (isTapRecognizerForTagretViewValue == TRUE) {
+        [materialShowcase setIsTapRecognizerForTagretView:TRUE];
+    }
+    
+    // Text
+    UIColor *primaryTextColor;
+    UIColor *secondaryTextColor;
+    //    showcase.primaryTextFont = UIFont.boldSystemFont(ofSize: primaryTextSize)
+    //    showcase.secondaryTextFont = UIFont.systemFont(ofSize: secondaryTextSize)
     
     NSString *primaryTextColorValue = [props objectForKey:@"primaryTextColor"];
     if (primaryTextColorValue != nil) {
@@ -191,28 +206,82 @@ RCT_EXPORT_METHOD(ShowFor:(nonnull NSNumber *)view props:(NSDictionary *)props)
     [materialShowcase setPrimaryText: primaryText];
     [materialShowcase setSecondaryText: secondaryText];
     
-    if (backgroundPromptColor != nil) {
-        [materialShowcase setBackgroundColor: backgroundPromptColor];
-    } if (targetTintColor != nil) {
-        target.tintColor = targetTintColor;
-        [materialShowcase setTargetTintColor: targetTintColor];
-    } if (targetHolderColor != nil) {
-        [materialShowcase setTargetHolderColor: targetHolderColor];
-    } if (primaryTextColor != nil) {
+    if (primaryTextColor != nil) {
         [materialShowcase setPrimaryTextColor: primaryTextColor];
     } if (secondaryTextColor != nil) {
         [materialShowcase setSecondaryTextColor: secondaryTextColor];
     }
     
-    BOOL *isTapRecognizerForTagretViewValue = [[props objectForKey:@"isTapRecognizerForTagretView"] boolValue];
-    if (isTapRecognizerForTagretViewValue == TRUE) {
-        [materialShowcase setIsTapRecognizerForTagretView:TRUE];
+    float primaryTextSizeValue = [[props objectForKey:@"primaryTextSize"] floatValue];
+    float secondaryTextSizeValue = [[props objectForKey:@"secondaryTextSize"] floatValue];
+    if (primaryTextSizeValue > 0) {
+        [materialShowcase setPrimaryTextSize: primaryTextSizeValue];
+    } if (secondaryTextSizeValue > 0) {
+        [materialShowcase setSecondaryTextSize: secondaryTextSizeValue];
+    }
+    
+    NSString *primaryTextAlignmentValue = [props objectForKey:@"primaryTextAlignment"];
+    NSString *secondaryTextAlignmentValue = [props objectForKey:@"secondaryTextAlignment"];
+    if (primaryTextAlignmentValue != nil) {
+        NSTextAlignment* primaryTextAlign = [self getTextAlignmentByString:primaryTextAlignmentValue];
+        [materialShowcase setSecondaryTextAlignment: primaryTextAlign];
+    } if (secondaryTextAlignmentValue != nil) {
+        NSTextAlignment* secondaryTextAlign = [self getTextAlignmentByString:secondaryTextAlignmentValue];
+        [materialShowcase setSecondaryTextAlignment: secondaryTextAlign];
+    }
+    
+    // Animation
+    float aniComeInDurationValue = [[props objectForKey:@"aniComeInDuration"] floatValue]; // second unit
+    float aniGoOutDurationValue = [[props objectForKey:@"aniGoOutDuration"] floatValue]; // second unit
+    if (aniGoOutDurationValue > 0) {
+        [materialShowcase setAniComeInDuration: aniComeInDurationValue];
+    } if (aniGoOutDurationValue > 0) {
+        [materialShowcase setAniGoOutDuration: aniGoOutDurationValue];
+    }
+    
+    UIColor *aniRippleColor;
+    NSString *aniRippleColorValue = [props objectForKey:@"aniRippleColor"];
+    if (aniRippleColorValue != nil) {
+        aniRippleColor = [UIColor fromHexWithHexString: aniRippleColorValue];
+    } if (aniRippleColor != nil) {
+        [materialShowcase setAniRippleColor: aniRippleColor];
+    }
+    
+    
+    if ([props objectForKey:@"aniRippleAlpha"] != nil) {
+        float aniRippleAlphaValue = [[props objectForKey:@"aniRippleAlpha"] floatValue];
+        if (aniRippleAlphaValue >= 0.0 && aniRippleAlphaValue <= 1.0) {
+            [materialShowcase setAniRippleAlpha: aniRippleAlphaValue];
+        }
+    }
+    
+    float aniRippleScaleValue = [[props objectForKey:@"aniRippleScale"] floatValue];
+    if (aniRippleScaleValue > 0) {
+        [materialShowcase setAniRippleScale:aniRippleScaleValue];
     }
     
     [materialShowcase setTargetViewWithView: target];
     [materialShowcase setDelegate: self];
     
     return materialShowcase;
+}
+
+- (NSTextAlignment*) getTextAlignmentByString: (NSString*) strAlignment {
+    if (strAlignment == nil) {
+        return NSTextAlignmentLeft; // default is left
+    }
+    
+    NSString *lowCaseString = [strAlignment lowercaseString];
+    if ([lowCaseString isEqualToString:@"left"]) {
+        return NSTextAlignmentLeft;
+    } if ([lowCaseString isEqualToString:@"right"]) {
+        return NSTextAlignmentRight;
+    } if ([lowCaseString isEqualToString:@"center"]) {
+        return NSTextAlignmentCenter;
+    } if ([lowCaseString isEqualToString:@"justify"]) {
+        return NSTextAlignmentJustified;
+    }
+    return NSTextAlignmentLeft;
 }
 
 @end
