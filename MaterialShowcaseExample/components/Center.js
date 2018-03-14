e4bcfa2211b60085be56c6f38964f0edb44f49eb
cong.nguyen <cong.nguyen@enouvo.com>
2018-03-14 15:46:57 +0700

    - update example with targetHolderRadius, alignment

--- MaterialShowcaseExample/components/Center.js
+++ MaterialShowcaseExample/components/Center.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
 
-import { AppTourView } from 'react-native-material-showcase-ios'
+import { AppTourView } from 'react-native-material-showcase-ios';
 
 class Center extends Component {
   render() {
@@ -10,55 +10,54 @@ class Center extends Component {
         <Button
           title={'Center Left'}
           ref={ref => {
-            this.props.addAppTourTarget && this.props.addAppTourTarget(AppTourView.for(
-                  ref,
-                  {
-                    primaryText: "This is a target button 3",
-                    secondaryText:
-                      "We have the best targets, believe me"
-                  }
-                ));
+            this.props.addAppTourTarget &&
+              this.props.addAppTourTarget(
+                AppTourView.for(ref, {
+                  primaryText: 'This is a target button 3',
+                  secondaryText: 'We have the best targets, believe me',
+                })
+              );
           }}
           onPress={() => {}}
         />
         <Button
           title={'Center Center'}
           ref={ref => {
-            this.props.addAppTourTarget && this.props.addAppTourTarget(AppTourView.for(
-                  ref,
-                  {
-                    primaryText: "This is a target button 4",
-                    secondaryText:
-                      "We have the best targets, believe me"
-                  }
-                ));
+            this.props.addAppTourTarget &&
+              this.props.addAppTourTarget(
+                AppTourView.for(ref, {
+                  primaryText: 'This is a target button 4',
+                  secondaryText: 'We have the best targets, believe me',
+                })
+              );
           }}
           onPress={() => {}}
         />
         <Button
           title={'Center Right'}
           ref={ref => {
-            this.props.addAppTourTarget && this.props.addAppTourTarget(AppTourView.for(
-                  ref,
-                  {
-                    primaryText: "This is a target button 5",
-                    secondaryText:
-                      "We have the best targets, believe me"
-                  }
-                ));
+            this.props.addAppTourTarget &&
+              this.props.addAppTourTarget(
+                AppTourView.for(ref, {
+                  primaryText: 'This is a target button 5',
+                  secondaryText:
+                    'We have the best targets, believe me\nsecondaryTextAlignment',
+                  secondaryTextAlignment: 'right',
+                })
+              );
           }}
           onPress={() => {}}
         />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-})
+    justifyContent: 'space-between',
+  },
+});
 
-export default Center
+export default Center;
